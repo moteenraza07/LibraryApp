19c0c89a7f59905e7dbdf440eed6a84d6c923b21
Moteen <moteenraza@gmail.com>
2025-12-06 14:55:00 -0500

    added comments for books class

--- Book.java
+++ Book.java
@@ -1,21 +1,30 @@
+// extend the abstract class from library item to inherit attributes
+// implement borrowable class
 public class Book extends LibraryItem implements Borrowable {
+    // name of the book's author
     private String author;
 
+    // constructor book id, string title, string author
     public Book (int id, String title, String author) {
-        super(id, title);
+        super(id, title); // initialize id and title from library item
         this.author = author;
     }
 
+    // display info for book
     @Override
     public void displayInfo() {
         System.out.println("Book: " + getTitle() + "by " + author);
     }
 
+    // calculate fee for book
+    // 0.50 charge per day late
     @Override
     public double calculateFee(int daysLate) {
         return daysLate * 0.50;
     }
 
+    // mark books borrowed
+    // throw exception if book is already borrowed
     @Override
     public void borrow() throws AlreadyBorrowedException {
         if(isBorrowed) {
@@ -25,11 +34,13 @@ public class Book extends LibraryItem implements Borrowable {
         setBorrowed(true);
     }
 
+    // mark book as returned
     @Override
     public void returnItem() {
         setBorrowed(false);
     }
 
+    // return book details
     @Override
     public String toString() {
         return "book " + super.toString() + "Author " + author;
